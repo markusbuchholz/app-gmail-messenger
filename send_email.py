83cff07ed06eb429a041eb1761988b57241ea5bd
markusbuchholz <markus.x.buchholz@gmail.com>
2021-01-20 01:34:58 +0100

    update

--- send_email.py
+++ send_email.py
@@ -2,6 +2,11 @@
 #2021
 import smtplib, ssl
 import streamlit as st
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+import os
 
 
 st.title ("WEB Gmail Messenger")
@@ -15,27 +20,48 @@ password = st.sidebar.text_input('your gmail password')
 
 receiver_email = st.sidebar.text_input('receiver email')
 
-def send_email(message, _password, _sender_email, _receiver_email):
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = _sender_email
-    receiver_email = _receiver_email
-    password = _password
+user_attachment = st.file_uploader('File uploader')
+
+def send_email(sender_email,receiver_email, SUBJECT, TEXT, password, user_attachment ):
+    print(user_attachment.name)
+    msg = MIMEMultipart() 
+    msg['From'] = sender_email 
+    msg['To'] = receiver_email 
+    msg['Subject'] = SUBJECT
+    body = TEXT
+  
+
+    msg.attach(MIMEText(body, 'plain')) 
     
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        try:
-            server.login(sender_email, password)
-            res = server.sendmail(sender_email, receiver_email, message)
-            print('email sent!')
-            st.text('email sent!')
-        except:
-            print("could not login or send the mail.")
+    filename = user_attachment.name #"File_name_with_extension"
+    #attachment = open(user_attachment, "rb")
+    attachment = user_attachment 
+    
+    p = MIMEBase('application', 'octet-stream') 
+    
+    p.set_payload((attachment).read()) 
+    
+    encoders.encode_base64(p) 
+   
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+  
+    msg.attach(p) 
+
+    try:
+        s = smtplib.SMTP('smtp.gmail.com', 587) 
+        s.starttls() 
+        s.login(sender_email, password)
+        text = msg.as_string() 
+        s.sendmail(sender_email, receiver_email, text)
+        print('email sent!')
+        st.text('email sent!')
+        s.quit() 
+    except:
+        print("could not login or send the mail.")
 
 clicked = st.button('Send email')
 
 if clicked:
   
-    message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
   
-    send_email(message, password, sender_email, receiver_email)
+    send_email(sender_email,receiver_email, SUBJECT, TEXT, password, user_attachment)
